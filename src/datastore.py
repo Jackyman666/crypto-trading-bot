89e634b3beda948a474de0e437c14feae01c6d29
jackyman666 <jackyman@connect.hku.hk>
2025-11-10 17:49:54 +0800

    update trade table

--- src/datastore.py
+++ src/datastore.py
@@ -77,13 +77,13 @@ class SQLiteDataStore:
         """
 
         with self._connect() as conn:
-            try:
-                conn.execute(
-                    "DROP TABLE IF EXISTS trades;"
-                )
-            except sqlite3.OperationalError:
-                # Column already exists; ignore error.
-                pass
+            # try:
+            #     conn.execute(
+            #         "DROP TABLE IF EXISTS trades;"
+            #     )
+            # except sqlite3.OperationalError:
+            #     # Column already exists; ignore error.
+            #     pass
             conn.executescript(schema)
 
     def fetch_pivots(
@@ -548,4 +548,3 @@ class SQLiteDataStore:
     #         conn.executemany(insert_sql, payload)
 
     #     return len(payload)
-
